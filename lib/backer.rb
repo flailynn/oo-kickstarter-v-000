41c49ea0c0666f01dbd5c92acae4147a7dba71e7
Anita Beauchamp <amadamspr@gmail.com>
2018-07-29 22:56:48 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,7 +8,7 @@ class Backer
   def back_project(project)
     backed_projects = self.backed_projects
     backed_projects << project unless backed_projects.include?(project)
-  
+    project.backers << self
     backed_projects
   end
 end
